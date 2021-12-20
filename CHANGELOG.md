ce54bca2fa193c726205f2689190e98c1eb88688
Nikolay Voynov <nvoynov@gmail.com>
2021-12-20 08:41:34 +0200

    refactores CloneCleonCode

--- CHANGELOG.md
+++ CHANGELOG.md
@@ -1,5 +1,9 @@
 ## [Unreleased]
 
+## [0.1.1] - 2021-12-20
+
+- Refactored CloneCleonCode
+
 ## [0.1.0] - 2021-12-06
 
 - Initial release
--- lib/cleon/services/clone_cleon_code.rb
+++ lib/cleon/services/clone_cleon_code.rb
@@ -19,11 +19,20 @@ module Cleon
         @base = @spec.sub('.gemspec', '').split(?-).first
         @clonesrc = File.join(@clonelib, "#{@base}.rb")
         @unit = @base.split(?_).map(&:capitalize).join
+
+        @cleonlib = File.join(Cleon.root, 'lib')
       end
 
       def call
-        cleonlib = File.join(Cleon.root, 'lib')
-        clone_tt = File.read(File.join(cleonlib, 'clone.rb.tt'))
+        create_clone_module
+        create_structure
+        path_and_copy_files
+      end
+
+      private
+
+      def create_clone_module
+        clone_tt = File.read(File.join(@cleonlib, 'clone.rb.tt'))
         # TODO: patch content @clonesrc when it exist
         if File.exist?(@clonesrc)
           clonebase = File.basename(@clonesrc)
@@ -32,16 +41,21 @@ module Cleon
           puts "Find original file in #{clonebase}~"
         end
         File.write(@clonesrc, clone_tt % {base: @base, clone: @unit})
+      end
+
+      def create_structure
         Dir.chdir(File.join(@clonelib, @base)) {
           %w(entities services gateways).each {|dir|
             Dir.mkdir(dir) unless Dir.exist?(dir)
           }
         }
+      end
 
-        what = Dir.chdir(cleonlib) { Dir.glob('**/*.rb') }
+      def path_and_copy_files
+        what = Dir.chdir(@cleonlib) { Dir.glob('**/*.rb') }
         what.delete_if{|s| s =~ /(clone_cleon_code|cleon).rb$/}
         what.each do |source|
-          content = File.read(File.join(cleonlib, source))
+          content = File.read(File.join(@cleonlib, source))
           clone_content = content
             .gsub("module #{Cleon.name}", "module #{@unit}")
             .gsub("#{Cleon.name}.", "#{@unit}.")
